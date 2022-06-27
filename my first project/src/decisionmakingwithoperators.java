7ef21fee12f37f258e789852370e95fd281beb21
ldmra <ldmra@MALLESWARARAO.bbrouter>
2022-06-27 11:57:03 +0530

    decisionmakingwithoperators

---
+++ my first project/src/decisionmakingwithoperators.java
@@ -0,0 +1,18 @@
+
+public class decisionmakingwithoperators {
+
+	public static void main(String[] args) 
+	{
+		  int x=5;
+   if(x!=5)
+   {
+	   System.out.println("value of x is not5");
+   }
+   else
+   {
+	   System.out.println("value of x is 5");
+   }
+
+	}
+
+}
