c74db7201d3f7260940ca7f80819af48c25b8375
ldmra <ldmra@MALLESWARARAO.bbrouter>
2022-06-27 10:51:52 +0530

    primitive datatype

---
+++ my first project/src/PrimitiveDatatype.java
@@ -0,0 +1,18 @@
+
+public class PrimitiveDatatype {
+
+	public static void main(String[] args)
+	{
+		int max=2147483647;
+		int min=-214783648;
+		short shortmax=32767;
+		short shortmin=-32768;
+		long longmax=9223372036854775807L;
+		long longmin=-9223372036854775808L;
+		byte bytemax=127;
+		byte bytemin=-128;
+		System.out.println(max);
+		
+	}
+
+}
